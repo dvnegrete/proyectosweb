0b1fc9c49646d7b9fa3bdc03d1b115e8b959a6e8
Damian Valenzuela Negrete <dvnegrete@gmail.com>
2024-08-16 20:40:26 -0600

    SEO y cambios Marca Personal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), sitemap()],
   site: 'https://proyectosweb.com.mx'
 });
